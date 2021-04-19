968192abf5895d4192eca1902e4da9878cf3ba32
Dudunaitor <82180142+Dudunaitor@users.noreply.github.com>
2021-04-19 15:20:29 -0300

    Add files via upload

---
+++ Logica de programação/Algoritimos para Entrega/Aula 1/Cartapramim.js
@@ -0,0 +1,31 @@
+//Escreva uma carta para você mesmo ler ao final do curso, e imprima no console. 
+//Na carta você deve incluir as suas expectativas para o curso de programação e sua carreira nesta nova área. 
+//Seja criativo, lembre-se de incluir desafios e incentivos, para que o seu eu do futuro tenha orgulho de suas conquistas!
+{
+    console.log("Bom... começou o curso e já não tive o começo que eu esperava. ");
+    console.log("Depois de alguns ano eu fiquei doente e o primeiro dia de aula eu estava muito mal, e foi assim pela semana. ");
+    console.log("Começando essa segunda semana, eu estou revisando as aulas da primeira semana que não deu para acompanhar direito. ");
+    console.log("Fazendo as atividades que o Zeca mandou, agora trabalhando em cima dos exercicios de algoritimos. ");
+    console.log("Ainda não tem nada de muito complexo, mas esta sendo bem divertido parar e escrever aqui. ");
+    console.log("Até que ta sendo relaxante, mas eu sei que é assim pq é o inicio ainda. ");
+    console.log("Mas depois da noticia que o que eu tive na primeira semana foi Covid... e que eu passei isso para minha mulher e provavelmente para minha filha. ");
+    console.log("ficar aqui revendo as aulas e praticando tem sido algo meu terapeutico. ");
+    console.log("O que é uma boa noticia, que passando essa tempestade de merda inicial que veio junto com o curso. ");
+    console.log("Quer dizer que o que é positivo vai permanecer. Não só o conhecimento que é impagavel mas tambem um prazer recem descoberto. ");
+    console.log("Pequenas ideias do que crirar começam a pipocar na minha cabeça, como um aplicativo para rolar dado num RPG.");
+    console.log("Mas um desejo que criar algo mais complexo pelo desafio tambem vem junto. ");
+    console.log("Com o tempo vou ter um nome forte na area, já que não pretendo parar de me atualizar.");
+    console.log("Quem sabe um nome internacional, quem sabe possa ter uma vida fora desse Brasilzão de meu deus. ");
+    console.log("É importante ter esses pequenos sonhos quando se começa um projeto assim. ");
+    console.log("Te animam a continuar, que por mais que eu tenha gostado do que vi até agora, sonhar com realizações são combustiveis exelentes. ");
+    console.log("E com o que tá rolando agora para mim... to precisando de combustivel kkkkk. ");
+    console.log("Mas sei que as coisas vão melhorar e o curso vai ser minha unica dificuldade. eu espero k. ");
+    console.log("Até lá a gente continua correndo a tras. sem desanimar porque a vida é assim mesma. ");
+    console.log("Te surra como se você fosse cachorro de rua. ");
+    console.log("E se você não tomar atitude é assim que vai ser. ");
+    console.log("Então sempre que estiver pensando em desistir espero que leia isso aqui denovo. ");
+    console.log("Por que o começo ja ta pedindo muito de você e mesmo assim vc ta aqui lutando. ");
+    console.log("E é assim que vc vai se formar nesse curso mesmo passando pelas partes mais dificeis. lutando. ");
+    console.log("Então levanta a cabeça, passa um café e vão bora! ");
+
+}
